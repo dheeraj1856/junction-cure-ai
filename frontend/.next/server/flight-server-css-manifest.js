9e8a7aa179c018dba36158e843659672fb0b76e7
dheeraj1856 <144531592+dheeraj1856@users.noreply.github.com>
2023-11-11 11:13:50 +0200

    add input modal

--- frontend/.next/server/flight-server-css-manifest.js
+++ frontend/.next/server/flight-server-css-manifest.js
@@ -1,4 +1,12 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {},
-  "cssModules": {}
+  "cssImports": {
+    "C:\\Users\\HP\\Documents\\Microsoft VS Code\\junction-cure-ai\\frontend\\app\\layout.tsx": [
+      "C:\\Users\\HP\\Documents\\Microsoft VS Code\\junction-cure-ai\\frontend\\styles\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\HP\\Documents\\Microsoft VS Code\\junction-cure-ai\\frontend\\app\\page": [
+      "C:\\Users\\HP\\Documents\\Microsoft VS Code\\junction-cure-ai\\frontend\\styles\\globals.css"
+    ]
+  }
 }
